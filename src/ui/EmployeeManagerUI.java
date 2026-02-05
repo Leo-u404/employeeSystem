224fae358909ee47642cb49470269f630f261a7c
Leo-u404 <yunlongli0403@gmail.com>
2026-02-05 19:50:31 +1100

    try2

---
+++ src/ui/EmployeeManagerUI.java
@@ -0,0 +1,113 @@
+package ui;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.Stack;
+
+public class EmployeeManagerUI {
+    private JFrame frame;
+    private JTable table;
+    private DefaultTableModel model;
+    private JTextField textFieldSearch;
+
+    public static void main(String[] args) {
+        EmployeeManagerUI window = new EmployeeManagerUI();
+        window.frame.setVisible(true);
+    }
+
+    public  EmployeeManagerUI() { initialize(); }
+
+    private void initialize() {
+        frame = new JFrame();
+        frame.setBounds(100,100,800,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().setLayout(new BorderLayout());
+
+        JPanel topPanel = new JPanel();
+        topPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        textFieldSearch = new JTextField(20);
+        JButton btnSearch = new JButton("Search");
+        JButton btnAdd = new JButton("Add");
+        topPanel.add(textFieldSearch);
+        topPanel.add(btnSearch);
+        topPanel.add(btnAdd);
+
+        model = new DefaultTableModel(
+                new Object[][]{},
+                new String[]{"ID", "NAME","POSITION","DEPARTMENT"}
+        ) {
+            @Override
+            public boolean isCellEditable(int row, int column) {return false;}
+        };
+
+        table = new JTable(model);
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.setRowHeight(30);
+
+        for (int i = 0; i < 20; i++) {
+            model.addRow(new Object[]{i + 1, "employee" + (i + 1), "position" + (i + 1), "department" + (i + 1),});
+        }
+
+        JPopupMenu popupMenu = new JPopupMenu();
+        JMenuItem editItem = new JMenuItem("edit");
+        JMenuItem deleteItem = new JMenuItem("delete");
+        popupMenu.add(editItem);
+        popupMenu.add(deleteItem);
+        table.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3) {
+                    int row = table.rowAtPoint(e.getPoint());
+                    if (row >= 0) {
+                        table.setRowSelectionInterval(row, row);
+                        popupMenu.show(table, e.getX(), e.getY());
+                    }
+                }
+            }
+        });
+
+        editItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selectedRow = table.getSelectedRow();
+                if (selectedRow >= 0){
+                    int id = (Integer) model.getValueAt(selectedRow, 0);
+                    JOptionPane.showMessageDialog(frame, "edit ID: " + id);
+                }
+            }
+        });
+
+        deleteItem.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int selectedRow = table.getSelectedRow();
+                if (selectedRow >= 0){
+                    int id = (Integer) model.getValueAt(selectedRow, 0);
+                    JOptionPane.showMessageDialog(frame, "delete ID: " + id);
+                }
+            }
+        });
+
+        btnSearch.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String searchValue = textFieldSearch.getText();
+            }
+        });
+
+        btnAdd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+
+        frame.getContentPane().add(topPanel, BorderLayout.NORTH);
+        frame.getContentPane().add(scrollPane, BorderLayout.CENTER);
+    }
+}
