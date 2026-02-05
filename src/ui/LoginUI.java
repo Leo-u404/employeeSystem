c5eb941439a9d91ca7099546199816c88d1c7663
Leo-u404 <yunlongli0403@gmail.com>
2026-02-05 18:20:25 +1100

    try2

---
+++ src/ui/LoginUI.java
@@ -0,0 +1,15 @@
+package ui;
+
+import javax.swing.*;
+
+public class LoginUI {
+    public static void main(String[] args) {
+        createAndShowGUI();
+    }
+
+    private static void createAndShowGUI() {
+        JFrame frame = new JFrame("LOGIN");
+    }
+
+
+}
